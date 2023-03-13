059749cf94712b8d8e99e3b8d110661fe607f4be
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2023-03-13 14:52:11 -0700

    Dark mode and light mode added

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav')
+
+
+const toggleIcon = document.getElementById('toggle-icon')
+const textBox = document.getElementById('text-box')
+
+
+// Images
+const image1 = document.getElementById('image1')
+const image2 = document.getElementById('image2')
+const image3 = document.getElementById('image3')
+
+
+
+// Dark Mode styles
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)'
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)'
+    toggleIcon.children[0].textContent = 'Dark Mode'
+    toggleIcon.children[1].classList.replace('fa-sun','fa-moon')
+    image1.src = 'img/undraw_feeling_proud_dark.svg'
+    image2.src = 'img/undraw_proud_coder_dark.svg'
+    image3.src = 'img/undraw_conceptual_idea_dark.svg'
+}
+
+
+// Light Mode Styles 
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)'
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)'
+    
+    toggleIcon.children[0].textContent = 'Light Mode'
+    toggleIcon.children[1].classList.replace('fa-moon','fa-sun')
+    image1.src = 'img/undraw_feeling_proud_light.svg'
+    image2.src = 'img/undraw_proud_coder_light.svg'
+    image3.src = 'img/undraw_conceptual_idea_light.svg'
+}
 
 // Switch theme Dyanamically
 function switchTheme(event){
     console.log(event.target.checked)
     if(event.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark')
+        darkMode()
     }else {
         document.documentElement.setAttribute('data-theme', 'light')
+        lightMode()
     }
 }
 
